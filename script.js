80148aa6b80b1987a960e490ccbf6a3225dbde3c
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-02-25 11:30:37 -0500

    Removed additional JS file, renamed

--- script.js
+++ script.js
@@ -1,160 +1,168 @@
-var numFlipped = 0;
-var card = document.querySelectorAll(".card-back");
-var cardOne = "";
-var cardTwo = "";
-var cardOneFont = "";
-var cardTwoFont = "";
-var matches = 0;
-var gameInProgress = true;
-var timer = document.querySelector(".timer");
-var colorSelect = document.querySelectorAll(".color-select");
-var fonts = ["one", "one", "two", "two", "three", "three", "four", "four", "five", "five", "six", "six"];
+var game = {
+  numFlipped: 0,
+  card: document.querySelectorAll(".card-back"),
+  cardOne: "",
+  cardTwo: "",
+  cardOneFont: "",
+  cardTwoFont: "",
+  matches: 0,
+  gameInProgress: true,
+  timer: document.querySelector(".timer"),
+  colorSelect: document.querySelectorAll(".color-select"),
+  fonts: ["one", "one", "two", "two", "three", "three", "four", "four", "five", "five", "six", "six"],
 
-startGame = function() {
-  for (var i=0; i<card.length; i++) {
-    card[i].addEventListener("click", yourTurn);
-  };
+  startGame: function() {
+    for (var i=0; i<this.card.length; i++) {
+      this.card[i].addEventListener("click", this.yourTurn);
+    };
 
-  timer.addEventListener("click", playTimedGame);
+    this.timer.addEventListener("click", this.playTimedGame);
 
-  for (var i=0; i<colorSelect.length; i++) {
-    colorSelect[i].addEventListener("click", changeCardColor);
-  };
-};
+    for (var i=0; i<this.colorSelect.length; i++) {
+      this.colorSelect[i].addEventListener("click", this.changeCardColor);
+    };
+  },
 
-shuffleFonts = function() {
-  fonts.forEach(function(font) {
-    fonts.sort(function() {
-      if (Math.random() > 0.5) {
-        return 1;
-      } else {
-        return -1;
-      };
+  shuffleFonts: function() {
+    var self = this;
+
+    self.fonts.forEach(function(font) {
+      self.fonts.sort(function() {
+        if (Math.random() > 0.5) {
+          return 1;
+        } else {
+          return -1;
+        };
+      });
     });
-  });
 
-  for (var i=0; i<fonts.length; i++) {
-    for (var i=0; i<card.length; i++) {
-      card[i].firstElementChild.dataset.font = fonts[i];
-    };
-  };
-};
-
-changeCardColor = function() {
-  var selectedColorBox = this.firstElementChild;
-
-  var newColor = selectedColorBox.getAttribute("data-color");
-
-  if (newColor === "deck-one") {
-    for (var i=0; i<card.length; i++) {
-      card[i].style.backgroundColor = "#3fb0ac";
-    };
-  } else if (newColor === "deck-two") {
-    for (var i=0; i<card.length; i++) {
-      card[i].style.backgroundColor = "#a2aff7";
-    };
-  } else if (newColor === "deck-three") {
-    for (var i=0; i<card.length; i++) {
-      card[i].style.backgroundColor = "#b4e9e7";
-    };
-  } else if (newColor === "deck-four") {
-    for (var i=0; i<card.length; i++) {
-      card[i].style.backgroundColor = "#e5e5e5";
-    };
-  };
-};
-
-yourTurn = function() {
-  var activeCard = this.firstElementChild;
-
-  if (numFlipped === 0) {
-    activeCard.classList.remove("inactive");
-
-    numFlipped++;
-
-    cardOne = activeCard;
-    cardOneFont = activeCard.getAttribute("data-font");
-
-  } else if (numFlipped === 1) {
-    activeCard.classList.remove("inactive");
-
-    numFlipped++;
-
-    cardTwo = activeCard;
-    cardTwoFont = activeCard.getAttribute("data-font");
-
-    checkAnswer();
-  };
-};
-
-checkAnswer = function() {
-  if (cardOneFont === cardTwoFont) {
-    var selectedCardOne = this.cardOne.parentNode;
-    var selectedCardTwo = this.cardTwo.parentNode;
-
-    setTimeout(function() {
-      selectedCardOne.classList.add("inactive");
-
-      selectedCardTwo.classList.add("inactive");
-
-      numFlipped = 0;
-
-      matches++;
-
-      winGame();
-
-    }, 1000);
-  } else {
-    setTimeout(function() {
-      cardOne.classList.add("inactive");
-
-      cardTwo.classList.add("inactive");
-
-      numFlipped = 0;
-
-    }, 1000);
-  };
-};
-
-playTimedGame = function() {
-  var loser = document.querySelector(".loser-outer");
-  var countdown = document.querySelector(".countdown");
-  var seconds = 29;
-  var second = 0;
-  var interval;
-
-  countdown.classList.remove("inactive");
-
-  if (gameInProgress) {
-    timer.style.display = 'none';
-
-    interval = setInterval(function() {
-      countdown.firstChild.data = (seconds - second);
-
-      if (second >= seconds && gameInProgress === false) {
-        countdown.classList.add("inactive");
-      } else if (second >= seconds) {
-        loser.classList.remove("inactive");
-
-        countdown.classList.add("inactive");
+    for (var i=0; i<self.fonts.length; i++) {
+      for (var j=0; j<self.card.length; j++) {
+        self.card[i].firstElementChild.dataset.font = self.fonts[i];
       };
-
-      second++;
-    }, 1000);;
-  };
-};
-
-winGame = function() {
-  var winner = document.querySelector(".winner-outer");
-
-  setTimeout(function() {
-    if (matches === 6) {
-      winner.classList.remove("inactive");
-
-      gameInProgress = false;
     };
-  }, 700);
+  },
+
+  changeCardColor: function() {
+    var selectedColorBox = this.firstElementChild;
+    var self = game;
+
+    var newColor = selectedColorBox.getAttribute("data-color");
+
+    var colorPairs = [
+      ["deck-one", "#3fb0ac"],
+      ["deck-two", "#a2aff7"],
+      ["deck-three", "#b4e9e7"],
+      ["deck-four", "#e5e5e5"]
+    ];
+
+    colorPairs.forEach(function(pair){
+      if(newColor === pair[0]){
+        for (var i=0; i<self.card.length; i++) {
+          self.card[i].style.backgroundColor = pair[1];
+        };
+      }
+    });
+  },
+
+  yourTurn: function() {
+    var activeCard = this.firstElementChild;
+    var self = game;
+
+    if (self.numFlipped === 0) {
+      activeCard.classList.remove("inactive");
+
+      self.cardOne = activeCard;
+      self.cardOneFont = activeCard.getAttribute("data-font");
+
+      self.numFlipped++;
+
+    } else if (self.numFlipped === 1 && self.cardOne != activeCard) {
+      activeCard.classList.remove("inactive");
+
+      self.numFlipped++;
+
+      self.cardTwo = activeCard;
+      self.cardTwoFont = activeCard.getAttribute("data-font");
+
+      self.checkAnswer();
+    };
+  },
+
+  checkAnswer: function() {
+    if (this.cardOneFont === this.cardTwoFont) {
+      var selectedCardOne = this.cardOne.parentNode;
+      var selectedCardTwo = this.cardTwo.parentNode;
+      var self = this;
+
+      setTimeout(function() {
+        selectedCardOne.classList.add("inactive");
+
+        selectedCardTwo.classList.add("inactive");
+
+        self.numFlipped = 0;
+
+        self.matches++;
+
+        self.winGame();
+
+      }, 1000);
+    } else {
+      var self = this;
+
+      setTimeout(function() {
+        self.cardOne.classList.add("inactive");
+
+        self.cardTwo.classList.add("inactive");
+
+        self.numFlipped = 0;
+
+      }, 1000);
+    };
+  },
+
+  playTimedGame: function() {
+    var loser = document.querySelector(".loser-outer");
+    var countdown = document.querySelector(".countdown");
+    var seconds = 29;
+    var second = 0;
+    var interval;
+    var self = game;
+
+    countdown.classList.remove("inactive");
+
+    if (self.gameInProgress) {
+      self.timer.style.display = 'none';
+
+      self.interval = setInterval(function() {
+        countdown.firstChild.data = (seconds - second);
+
+        if (second >= seconds) {
+          countdown.classList.add("inactive");
+
+          if (self.gameInProgress) {
+            loser.classList.remove("inactive");
+          };
+        };
+
+        second++;
+      }, 1000);;
+    };
+  },
+
+  winGame: function() {
+    var winner = document.querySelector(".winner-outer");
+    var self = this;
+
+    setTimeout(function() {
+      if (self.matches === 6) {
+        winner.classList.remove("inactive");
+
+        self.gameInProgress = false;
+      };
+    }, 700);
+  }
 };
 
-startGame();
-shuffleFonts();
+game.startGame();
+game.shuffleFonts();
